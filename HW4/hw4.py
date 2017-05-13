7980d855eda7e4552795587173ad133e929dd8cd
Dvir Segal <dvirsegal84@gmail.com>
2017-05-13 19:19:16 +0300

    Implement second question and re-factor code
    1. Arrange first question code
    2. Add  code for second question

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,12 +1,17 @@
 import os
-# from gensim.models import Word2Vec
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import MaxAbsScaler
 import codecs
 import nltk
+import numpy as np
 from sys import argv
 
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as elementTree
 
 INSTANCE = ".//instance"
 
@@ -34,8 +39,8 @@ def get_full_data(data, labels):
     """
     Return concatenations of the data (all DB)
     second list contains all the labels
-    :param data:
-    :param labels:
+    :param data: list of list of data to concat
+    :param labels: list of all labels according to given data
     :return: 
     """
     full_data = []
@@ -50,6 +55,11 @@ def get_full_data(data, labels):
 
 
 def get_data(data):
+    """
+    Get the categories data
+    :param data: xml tree stracture
+    :return: cord, division, formation, phone, product arrays
+    """
     cord = []
     division = []
     formation = []
@@ -63,7 +73,7 @@ def get_data(data):
         for sentence in inst[1].getchildren():
 
             tokens = nltk.word_tokenize(sentence.text)
-            tokens = ' '.join(tokens).lower()
+            tokens = ' '.join(tokens).replace("<s>","").replace("<\s>","").lower()
             if att == CORD:
                 cord.append(tokens)
             elif att == DIVISION:
@@ -78,6 +88,126 @@ def get_data(data):
     return cord, division, formation, phone, product
 
 
+def print_scores(scores):
+    for score in scores:
+        print(str(score[0]) + ": " + str(round(score[1], 2)) + '\n')
+
+
+def first_question(test_tree, train_tree):
+    """
+    First question: implement logistic regression to recognize "line" category
+    :param test_tree: test data , xml structure  
+    :param train_tree: train data, xml structure
+    :return: 
+    """
+
+    # Get train data by classification
+    cord, division, formation, phone, product = get_data(train_tree)
+
+    # Build train data and labels arrays
+    full_labels, full_data = get_full_data([cord, division, formation, phone, product],
+                                           [CORD_LBL, DIVISION_LBL, FORMATION_LBL, PHONE_LBL, PRODUCT_LBL])
+
+    # Create bag of words using TfidfVectorizer
+    vectorizer = TfidfVectorizer(min_df=1, stop_words='english')
+    X = vectorizer.fit_transform(full_data, full_labels)
+
+    # Run logistic regression
+    model = LogisticRegression()
+    model.fit(X, full_labels)
+
+    # Get test data by classification
+    cord, division, formation, phone, product = get_data(test_tree)
+
+    # Build test data and labels arrays
+    full_test_labels, full_test_data = get_full_data([cord, division, formation, phone, product],
+                                                     [CORD_LBL, DIVISION_LBL, FORMATION_LBL, PHONE_LBL, PRODUCT_LBL])
+    # Trasnform test data to same shape of train data
+    X_test = vectorizer.transform(full_test_data, full_test_labels)
+
+    # Predict using logistic regression
+    y_predict = model.predict(X_test)
+
+    # Flatten full_test_labels, this are the y (true) labels
+    y = np.ravel(full_test_labels)
+
+    # Get f1_score and accuracy
+    f_score = f1_score(y, y_predict, average='macro')
+    accuracy = accuracy_score(y, y_predict)
+    return accuracy, f_score
+
+
+def second_question(test_tree, train_tree):
+    """
+    Second question: 
+    :param test_tree: test data , xml structure  
+    :param train_tree: train data, xml structure
+    :return: 
+    """
+
+    # Get train data by classification
+    cord, division, formation, phone, product = get_data(train_tree)
+
+    # Build train data and labels arrays
+    full_labels, full_data = get_full_data([cord, division, formation, phone, product],
+                                           [CORD_LBL, DIVISION_LBL, FORMATION_LBL, PHONE_LBL, PRODUCT_LBL])
+
+    # Load word2vec embeddings file
+    result = KeyedVectors.load_word2vec_format("wiki.en.100k.vec", binary=False)
+
+    # Get train data features by word2vec
+    X_train = get_features(full_data, result)
+
+    # Normalize features
+    scaler = MaxAbsScaler()
+    X_train_maxabs = scaler.fit_transform(X_train)
+
+    # Run logistic regression
+    model = LogisticRegression()
+    model.fit(X_train_maxabs, full_labels)
+
+    # Get test data by classification
+    cord, division, formation, phone, product = get_data(test_tree)
+
+    # Build test data and labels arrays
+    full_test_labels, full_test_data = get_full_data([cord, division, formation, phone, product],
+                                                     [CORD_LBL, DIVISION_LBL, FORMATION_LBL, PHONE_LBL, PRODUCT_LBL])
+
+    # Get test data features by word2vec
+    X_test = get_features(full_test_data, result)
+
+    # Normalize features
+    X_test_maxabs = scaler.transform(X_test)
+
+    # Predict using logistic regression
+    y_predict = model.predict(X_test_maxabs)
+
+    # Flatten full_test_labels, this are the y (true) labels
+    y = np.ravel(full_test_labels)
+
+    # Get f1_score and accuracy
+    f_score = f1_score(y, y_predict, average='macro')
+    accuracy = accuracy_score(y, y_predict)
+    return accuracy, f_score
+
+
+def get_features(full_data, result):
+    X_train = np.zeros([len(full_data), result.syn0.shape[1]])
+    for i, sentence in enumerate(full_data):
+        sum_sentence = np.zeros(result.syn0.shape[1])
+        sentence = sentence.split(" ")
+        k = len(sentence)
+        for w1 in sentence:
+            if w1 in result.index2word:
+                w_i = 1
+                index = result.index2word.index(w1)
+                v_i = result.syn0[index]
+                sum_sentence += (w_i * v_i) / k
+        X_train[i] = sum_sentence
+
+    return X_train
+
+
 def main():
     # Check for expected number of Arguments
     if len(argv) != number_of_args:
@@ -90,29 +220,18 @@ def main():
     if not os.path.exists(output):
         os.makedirs(output)
 
-    train_tree = ET.parse(codecs.open(train_file_path, 'r+', 'utf-8'))
-    test_tree = ET.parse(codecs.open(test_file_path, 'r+', 'utf-8'))
+    # Parse train and tree data using xml.etree.ElementTree
+    train_tree = elementTree.parse(codecs.open(train_file_path, 'r+', 'utf-8'))
+    test_tree = elementTree.parse(codecs.open(test_file_path, 'r+', 'utf-8'))
 
-    cord, division, formation, phone, product = get_data(train_tree)
+    accuracy, f_score = first_question(test_tree, train_tree)
 
-    # Create bag of words using TfidfVectorizer
-    full_labels, full_data = get_full_data([cord, division, formation, phone, product],
-                                           [CORD_LBL, DIVISION_LBL, FORMATION_LBL, PHONE_LBL, PRODUCT_LBL])
+    print('\n' + "classification using BOW model:" + '\n')
+    print_scores([['accuracy', accuracy], ['f1-score', f_score]])
 
-    vectorizer = TfidfVectorizer(min_df=1, stop_words='english')
-    features = vectorizer.fit_transform(full_data)
-
-    logit = LogisticRegression()
-    logit.fit(features, full_labels)
-
-    cord, division, formation, phone, product = get_data(test_tree)
-    full_test_labels, full_test_data = get_full_data([cord, division, formation, phone, product],
-                                           [CORD_LBL, DIVISION_LBL, FORMATION_LBL, PHONE_LBL, PRODUCT_LBL])
-
-    test_vectorizer = TfidfVectorizer(min_df=1, stop_words='english')
-    test_features = test_vectorizer.fit_transform(full_test_data)
-
-    logit.predict(test_features)
+    accuracy, f_score = second_question(test_tree, train_tree)
+    print('\n' + "classification using embeddings:" + '\n')
+    print_scores([['accuracy', accuracy], ['f1-score', f_score]])
 
     pass
 
